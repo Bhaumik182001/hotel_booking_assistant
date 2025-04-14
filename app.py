9ec55ae836b7412fedad76529f8acab259fb2d39
Bhaumik182001 <Bhaumik182001@gmail.com>
2025-04-15 01:59:45 +0530

    Update app.py
    Fixed port 0.0.0.0

--- app.py
+++ app.py
@@ -224,4 +224,6 @@ def chat():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
